b553f986d2e0f986b9da2ff5b4ba62f0ab3afb42
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-11-04 15:52:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
-wjbdvjwbvwjbv
+provider "aws" {
+  region     = "ap-south-1"
+  
+}
+# Create a VPC for the region associated with the AZ
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = "network-${var.infra_env}-vpc"
+    Environment = var.infra_env
+
+  }
+}
+
+
+
+
